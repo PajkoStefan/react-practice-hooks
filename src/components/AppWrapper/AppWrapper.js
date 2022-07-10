990f8008b51b590ade07a20df829885c9c921e0d
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-07-10 19:44:25 +0200

    Initial Commit

---
+++ src/components/AppWrapper/AppWrapper.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import MainHeader from '../MainHeader/MainHeader'
+import Login from '../Login/Login'
+import Home from '../Home/Home'
+
+function AppWrapper() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default AppWrapper;
